65ce3434a2047d8126302ccef09eb0534e89ded9
FranST724 <fran.tasinazzo@gmail.com>
2021-10-13 23:59:36 -0300

    Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+const NavBar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="" class="pseudo">Home</a>
+                    </li>
+                    <li>
+                        <a href="" class="pseudo">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="" class="pseudo">Productos</a>
+                    </li>
+                    <li>
+                        <a href="" class="pseudo">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar
